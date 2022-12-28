3e649ef1ede91aa37f76600f521e1cc2f784ee86
agtTwilight <lukasmacmillen@gmail.com>
2022-12-28 13:31:18 -0800

    timer & deduction system added

--- assests/script.js
+++ assests/script.js
@@ -1,7 +1,11 @@
 const boxEl =  document.querySelectorAll(".box");
-const initialDisplay = document.querySelector("#start");
 const finalDisplay = document.querySelector("#final")
-var currentDisplay = initialDisplay;
+var currentDisplay = document.querySelector("#start");
+var deductions = document.querySelector("#deductions")
+var timeCount = document.querySelector("span")
+
+// has to be globally scoped so two functions can update it at once
+var secondsLeft = 101
 
 // Gives me a list of index positions in boxEl that have not been displayed
 var displaysAvailable = []
@@ -10,31 +14,59 @@ for (i = 1; i < boxEl.length; i ++) {
 }
 
 // On button press, start the quiz
-// TODO add timer start feature within the code block.
-initialDisplay.children[1].addEventListener("click", function(event){
-        initialDisplay.setAttribute("style", "display: none;")
-        boxEl[displaysAvailable[0]].setAttribute("style", "display: flex;")
-        currentDisplay = boxEl[displaysAvailable[0]]
-        displaysAvailable.shift()
+currentDisplay.children[1].addEventListener("click", function(event){
+        transitionDisplay()
+        quizStart()
+        timer()
 })
 
+// Moves you to the next unused display
+function transitionDisplay() {
+        currentDisplay.setAttribute("style", "display: none;")
+        currentDisplay = boxEl[displaysAvailable[0]]
+        currentDisplay.setAttribute("style", "display: flex;")
+        displaysAvailable.shift()
+}
+
 // Click function to move to the next question
-for (i = 1; i < boxEl.length -1; i ++) {
-        boxEl[i].addEventListener("click", function(event) {
-                var element = event.target;
-                console.log(element)
-        
-                // TODO add T/F attribute checker & time deductions feature (on F selection)
-                // TODO add feedback on answer click ("incorrect."/"correct!")
-                if (element.matches("li")){
-                        currentDisplay.setAttribute("style", "display: none;");
-                        boxEl[displaysAvailable[0]].setAttribute("style", "display: flex;")
-                        currentDisplay = boxEl[displaysAvailable[0]]
-                        displaysAvailable.shift()
+function quizStart() {
+        for (i = 1; i < boxEl.length -1; i ++) {
+                boxEl[i].addEventListener("click", function(event) {
+                        var element = event.target;
+                        console.log(element)
+                
+                        // TODO add feedback on answer click ("incorrect."/"correct!")
+                        if (element.matches("li")){
+                                // check if their answer was correct
+                                if (element.getAttribute("data-answer") === "false") {
+                                        secondsLeft = secondsLeft-10
+                                        deductions.textContent = "-10"
+                                }
+                                transitionDisplay()
+                        }
+                })
+        }
+}
+
+function timer() {
+        // var secondsLeft = 101;
+        var timerInterval = setInterval(function() {
+                secondsLeft--;
+                timeCount.textContent = secondsLeft;
+                console.log(secondsLeft)
+
+                // in case they finish
+                if (displaysAvailable.length === 0) {
+                        clearInterval(timerInterval);
                 }
-        })
+
+                // in case they dont finish
+                if (secondsLeft === 0) {
+                        // TODO make failiure screen pop up
+                }
+        }, 1000);
 }
 
 // TODO add event listener for quiz submission on final box.
 // TODO add emoji effects on click during completion screen (for pass and fail instances)
-
+// TODO add randomizer option for question & answer order (select boxes that pull out on div hover)
