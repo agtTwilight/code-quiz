3be0bca4eacfc8276420b2a29f1d1e21e0f2425a
agtTwilight <lukasmacmillen@gmail.com>
2022-12-28 10:16:59 -0800

    display cycling on click

--- assests/script.js
+++ assests/script.js
@@ -1,27 +1,40 @@
-const boxEl =  document.querySelectorAll(".box")
-currentDisplay = displayContent()
+const boxEl =  document.querySelectorAll(".box");
+const initialDisplay = document.querySelector("#start");
+const finalDisplay = document.querySelector("#final")
+var currentDisplay = initialDisplay;
 
-// it might be better to set the start ID to visible and all boxes to display none by default in css. Then you can use ID to make start be the initial display variable, and use one function.
-// TODO set box objects to display none as defualt css & set ID start to display flex by default in css.
-// TODO change the following function so it doesn't iterate through all boxEl's whenever it is called (that's not efficient!). Instead, make the update function be called on click (whenever a click happens, update the current display to display none, and update the next display to display flex). To keep track of the displays you've used between clicks, you will need to create a global variable that is an array of indexs the length of boxEl. Then, each time a click happens, your function will also need to apply .shift() (pops the first value on an array) to update the "available displays".
-
-function displayContent() {
-        for (i = 0; i < boxEl.length; i++) {
-                var state = boxEl[i].getAttribute("data-state")
-                if (state === "hidden") {
-                        boxEl[i].setAttribute("style", "display: none;")
-                } else {
-                        boxEl[i].setAttribute("style", "display: flex;")
-                        var visible = boxEl[i]
-                }
-        }
-        return visible
+// Gives me a list of index positions in boxEl that have not been displayed
+var displaysAvailable = []
+for (i = 1; i < boxEl.length; i ++) {
+        displaysAvailable.push(i)
 }
 
-// This function will be called on section click to update the data-state of the current display to hidden, and set a new display to visible. It then calls the display content function to update the page. Will need to be update to go through an array of boxEl questions.
-
-boxEl[0].addEventListener("click", function updateContent() {
-        currentDisplay.setAttribute("data-state", "hidden")
-        boxEl[1].setAttribute("data-state", "visible")
-        displayContent()
+// On button press, start the quiz
+// TODO add timer start feature within the code block.
+initialDisplay.children[1].addEventListener("click", function(event){
+        initialDisplay.setAttribute("style", "display: none;")
+        boxEl[displaysAvailable[0]].setAttribute("style", "display: flex;")
+        currentDisplay = boxEl[displaysAvailable[0]]
+        displaysAvailable.shift()
 })
+
+// Click function to move to the next question
+for (i = 1; i < boxEl.length -1; i ++) {
+        boxEl[i].addEventListener("click", function(event) {
+                var element = event.target;
+                console.log(element)
+        
+                // TODO add T/F attribute checker & time deductions feature (on F selection)
+                // TODO add feedback on answer click ("incorrect."/"correct!")
+                if (element.matches("li")){
+                        currentDisplay.setAttribute("style", "display: none;");
+                        boxEl[displaysAvailable[0]].setAttribute("style", "display: flex;")
+                        currentDisplay = boxEl[displaysAvailable[0]]
+                        displaysAvailable.shift()
+                }
+        })
+}
+
+// TODO add event listener for quiz submission on final box.
+// TODO add emoji effects on click during completion screen (for pass and fail instances)
+
